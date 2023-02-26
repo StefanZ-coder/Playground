b60537e85d05b58666c95696f1531d9e74ff7083
Stefan <stefanzimny25@gmail.com>
2023-02-26 21:23:31 +0100

    Start/Stop Funktion und Ball erkennt den Rand des Feldes

--- Playground/MainWindow.xaml.cs
+++ Playground/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Playground
 {
@@ -20,14 +21,75 @@ namespace Playground
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly DispatcherTimer _animationsTimer = new DispatcherTimer();
+        private bool goRightLeft = true;
+        private bool goUpDown = true;
+
+        private int count = 0;
         public MainWindow()
         {
             InitializeComponent();
+            _animationsTimer.Interval = TimeSpan.FromMilliseconds(50);
+            _animationsTimer.Tick += MovingBall;
+        }
+
+        private void MovingBall(object? sender, EventArgs e)
+        {
+            var x = Canvas.GetLeft(Ball);
+
+            if (goRightLeft)
+            {
+                Canvas.SetLeft(Ball, x + 5);
+            }
+            else
+            {
+                Canvas.SetLeft(Ball, x - 5);
+            }
+
+            if(x >= FootballField.Width - Ball.ActualWidth)
+            {
+                goRightLeft = false;
+            }
+            else if(x <= 0)
+            {
+              goRightLeft = true;
+            }
+
+
+            var y = Canvas.GetTop(Ball);
+
+            if (goUpDown)
+            {
+                Canvas.SetTop(Ball, y + 5);
+            }
+            else
+            {
+                Canvas.SetTop(Ball, y - 5);
+            }
+
+            if (y >= FootballField.ActualHeight - Ball.ActualHeight)
+            {
+                goUpDown = false;
+            }
+            else if (y <= 0)
+            {
+                goUpDown = true;
+            }
+
         }
 
         private void StartStopButton_Click(object sender, RoutedEventArgs e)
         {
-
+            if(_animationsTimer.IsEnabled)
+            {
+                _animationsTimer.Stop();
+            }
+            else
+            {
+                _animationsTimer.Start();
+                count = 0;
+                CountLabel.Content = $"{count} Clicks";
+            }
         }
     }
 }
